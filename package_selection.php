91d1c39546583de51ba93dace601947d62dfe549
mtnaidu <naidu@ladatechnologies.com>
2017-07-04 18:27:02 +0530

    package selection design

--- package_selection.php
+++ package_selection.php
@@ -5,14 +5,15 @@ $package_type = $_REQUEST['type'];
  Hero Section 
 
 <div class="contact">
-    <div class="container inner-padding7" style="margin-top:100px;">       
+    <div class="container inner-padding5">       
         <div class="row">
-            <h3>Your Are Selected <?PHP echo $package_type ?> Package </br>Please Fill The Following Details</h3>
+            
             <div class="col-md-4 col-sm-12 col-xs-12">                
             </div>
-            <div class="col-md-4 col-sm-12 col-xs-12 well">
-                <h3>Your Choosen <?PHP // echo $package_type ?> Please Fill The Following Details</h3>
-                <h3>Contact US</h3>
+            <div class="col-md-5 col-sm-12 col-xs-12">
+                <h3 class="selectH">Your Are Selected<Br> <span><?PHP echo $package_type ?> Package</span> </br>Please Fill The Following Details</h3>
+                <div class="well">
+                 
                 <?php
                 if (isset($_GET['success'])) {
                     echo "<font color=green>thanks for contacting us<br>we will get back soon...</font>";
@@ -21,25 +22,39 @@ $package_type = $_REQUEST['type'];
                     echo "<font color=red>Check the mail..may be some thig is missig..</font>";
                 }
                 ?>
-                <form id="contactUsForm" name="contactUsForm" class="cmxform" method="post" action="contact_us.php">                    
-                    <div class="form-group col-md-6">
-                        <label for="name">Name:</label>
+                <form id="contactUsForm" name="contactUsForm" class="cmxform form-horizontal" method="post" action="contact_us.php">                    
+                    <div class="form-group">
+                        <label for="name" class="col-sm-4 control-label">Name:</label>
+                        <div class="col-sm-8">
                         <input type="text" class="form-control" id="firstname" name="name" placeholder="name">
+                        </div>
                     </div>                    
-                    <div class="form-group col-md-6">
-                        <label for="email">Email:</label>
+                    <div class="form-group">
+                        <label for="email" class="col-sm-4 control-label">Email:</label>
+                        <div class="col-sm-8">
                         <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="phoneno">Phone Number </label>
+                    <div class="form-group">
+                        <label for="phoneno" class="col-sm-4 control-label">Phone Number </label>
+                        <div class="col-sm-8">
                         <input type="text" class="form-control" id="phoneno" name="phoneno"  placeholder="Phone Number">
+                        </div>
                     </div>
-                    <div class="form-group col-md-12">
-                        <label for="exampleInputPassword1">Message</label>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1" class="col-sm-4 control-label">Message</label>
+                        <div class="col-sm-8">
                         <textarea class="form-control" rows="2" id="message" name="message" placeholder="Message"></textarea>
+                        </div>
                     </div>
-                    <input class="submit btn btn-theme"  id="sendmessage" name="sendmessage" type="submit" value="Send Message">
+                    <div class="form-group">
+                        <label for="exampleInputPassword1" class="col-sm-4 control-label"></label>
+                        <div class="col-sm-8">
+                    <input class="submit btn btn-success"  id="sendmessage" name="sendmessage" type="submit" value="Send Message">
+                        </div>
+                    </div>
                 </form>
+                </div>
             </div>
         </div>
 
@@ -48,7 +63,7 @@ $package_type = $_REQUEST['type'];
 
 </div>
 
- End Service Section 
+  
 
 <?php include_once('footer.php') ?>
 <script type="text/javascript" src="js/jquery.validate.js"></script>
